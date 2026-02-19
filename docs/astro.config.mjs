1a3db84e63e075d942185d58d824ebf0ed8d30cb
Paul Tibbetts <code@paultibbetts.uk>
2026-02-19 18:19:43 +0000

    feat: add webmention link pointing to webmention.io

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,42 +6,53 @@ import d2 from 'astro-d2';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'infra.paultibbetts.uk',
-    social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/paultibbetts/infra.paultibbetts.uk' }],
-    sidebar: [
-      { label: 'Index', link: '/' },
-      {
-        label: 'Architecture',
-        items: [
-          { label: 'Overview', slug: 'architecture/overview' },
-          { label: 'Network', slug: 'architecture/network' },
-          { label: 'Hardware', slug: 'architecture/hardware' },
-          { label: 'Software', slug: 'architecture/software' },
-        ],
-      },
-      {
-        label: 'Automation',
-        items: [
-          { label: 'Overview', slug: 'automation/overview' },
-          { label: 'Terraform', slug: 'automation/terraform' },
-          { label: 'Ansible', slug: 'automation/ansible' },
-          { label: 'CD', link: 'automation/cd' },
-          { label: 'Workflow', link: 'automation/workflow' },
-        ],
-      },
-      {
-        label: 'Decisions',
-        autogenerate: { directory: 'decisions' },
-      },
-      { label: 'Code', link: '/code' },
+  integrations: [
+    starlight({
+      title: 'infra.paultibbetts.uk',
+      head: [
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'webmention',
+            href: 'https://webmention.io/paultibbetts.uk/webmention',
+          }
+        }
+      ],
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/paultibbetts/infra.paultibbetts.uk' }],
+      sidebar: [
+        { label: 'Index', link: '/' },
+        {
+          label: 'Architecture',
+          items: [
+            { label: 'Overview', slug: 'architecture/overview' },
+            { label: 'Network', slug: 'architecture/network' },
+            { label: 'Hardware', slug: 'architecture/hardware' },
+            { label: 'Software', slug: 'architecture/software' },
+          ],
+        },
+        {
+          label: 'Automation',
+          items: [
+            { label: 'Overview', slug: 'automation/overview' },
+            { label: 'Terraform', slug: 'automation/terraform' },
+            { label: 'Ansible', slug: 'automation/ansible' },
+            { label: 'CD', link: 'automation/cd' },
+            { label: 'Workflow', link: 'automation/workflow' },
+          ],
+        },
+        {
+          label: 'Decisions',
+          autogenerate: { directory: 'decisions' },
+        },
+        { label: 'Code', link: '/code' },
 
-    ],
-  }), d2({
-    layout: "elk",
-    sketch: true,
-    experimental: {
-      useD2js: true,
-    },
-  })],
+      ],
+    }),
+    d2({
+      layout: "elk",
+      sketch: true,
+      experimental: {
+        useD2js: true,
+      },
+    })],
 });
